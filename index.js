a379544e6166020cab52254b7b667a5ed622f9c6
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-10-06 22:14:15 +0300

    progress

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+// const vision = require('@google-cloud/vision');
+// // Creates a client
+// const client = new vision.ImageAnnotatorClient({
+//     keyFilename: 'APIKey.json'
+// });
+
+// // Performs label detection on the image file
+// client
+//     .labelDetection('./IMG_20191214_155013.jpg')
+//     .then(results => {
+//         const labels = results[0].labelAnnotations;
+//         let descriptionTags = [];
+//         console.log('Labels:');
+//         labels.forEach(label => descriptionTags.push(label.description));
+//         console.log(descriptionTags);
+//     })
+//     .catch(err => {
+//         console.error('ERROR:', err);
+//     });
+
+// app.listen(5000, '127.0.0.1', () => console.log('Server running'));
+
 const vision = require('@google-cloud/vision');
-// Creates a client
+const server = require('./lib/server');
+
 const client = new vision.ImageAnnotatorClient({
     keyFilename: 'APIKey.json'
 });
 
-// Performs label detection on the image file
-client
-    .labelDetection('./IMG_20191214_155013.jpg')
-    .then(results => {
-        const labels = results[0].labelAnnotations;
+const app = {}
 
-        console.log('Labels:');
-        labels.forEach(label => console.log(label));
-        //console.log(results);
-    })
-    .catch(err => {
-        console.error('ERROR:', err);
-    });
+app.init = () => {
+    client
+        .labelDetection('./IMG_20191214_155013.jpg')
+        .then(results => {
+            const labels = results[0].labelAnnotations;
+            let descriptionTags = [];
+            console.log('Labels:');
+            labels.forEach(label => descriptionTags.push(label.description));
+            console.log(descriptionTags);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
+    // paruosti reikiamus direktorijas
 
-app.listen(5000, '127.0.0.1', () => console.log('Server running'));
+    // paruosti reikiamus failus
+
+    // inicijuojame serveri
+    server.init();
+}
+
+app.init();
+
+module.exports = app;
